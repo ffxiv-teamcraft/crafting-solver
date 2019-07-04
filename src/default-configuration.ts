c6c80537e314e6a55c740244d98efb115e655957
Supamiu <contact@flavien-normand.fr>
2019-07-04 12:38:46 +0200

    fix: fixed default weights for better results

--- src/default-configuration.ts
+++ src/default-configuration.ts
@@ -3,12 +3,12 @@ import { SolverConfiguration } from './solver-configuration';
 export const defaultConfiguration: SolverConfiguration = {
   populationSize: 200,
   iterations: 300,
-  progressAccuracy: 5,
-  safe: false,
+  progressAccuracy: 10,
+  safe: true,
   weights: {
     finished: 10,
-    progress: 5,
+    progress: 8,
     hq: 50,
-    length: 10
+    length: 5
   }
 };
